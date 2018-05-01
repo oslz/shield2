ba9ac2baa234dcff4147d0a067eeab1506984f85
oslz <38636929+oslz@users.noreply.github.com>
2018-05-01 00:44:34 -0300

    Add files via upload

--- comandos/casal.js
+++ comandos/casal.js
@@ -1,7 +1,10 @@
 
 const Discord = require(`discord.js`);
 const client = new Discord.Client();
-exports.run = (client, message, args)  => {
+const Canvas = require('canvas-prebuilt');
+const pixelUtil = require('pixel-util');
+const urlCoracao = 'https://cdn.discordapp.com/attachments/433776595187269633/436281835186683904/a1cd2e4a82bf9407d751f02f81baf257.png';
+
     var falas = [
         "10% [█.........]", 
         "20% [██........]", 
@@ -14,13 +17,37 @@ exports.run = (client, message, args)  => {
         "70% [███████...]", 
         "100%[██████████]"
     ];
-    mentions = message.mentions.users.first(2);
-    if (mentions < 2) return message.reply("**Mencione as duas pessoas**");
-    embed = new Discord.RichEmbed()
-        .setTitle(`💫 ${message.guild.name} 💫`)
-        .setColor(2490112)
-        .setTimestamp(new Date())
-        .addField('Casal escolhido foi', `${mentions[0]} ${mentions[1]}`)
-        .addField('Chances atuais', `${falas[Math.floor(Math.random() * falas.length - 1)]}`, true)
-        message.channel.send(embed).then(msg => {msg.delete(15000)});
-}
+
+        exports.run = (client, message, args) => {
+            let canvas   = new Canvas(384, 128),
+                ctx      = canvas.getContext('2d'),
+                mentions = message.mentions.users.first(2);
+                
+            pixelUtil.createBuffer(mentions[0].avatarURL).then(buffer1 => {
+                let img1 = new Canvas.Image;
+                img1.src = buffer1;
+                ctx.drawImage(img1, 0, 0, 128, 128);
+                pixelUtil.createBuffer(urlCoracao).then(buffer2 =>  {
+                    let img2 = new Canvas.Image;
+                    img2.src = buffer2;
+                    ctx.drawImage(img2, 128, 0, 128, 128);
+                    pixelUtil.createBuffer(mentions[1].avatarURL).then(buffer3 => {
+                        let img3 = new Canvas.Image;
+                        img3.src = buffer3;
+                        ctx.drawImage(img3, 256, 0, 128, 128);
+                        let file  = new Discord.Attachment(canvas.toBuffer(), 'shipp.png'),
+                            embed = new Discord.RichEmbed()
+                                .setTitle(`**Será que temos um casal aqui**?`)
+                                .setColor(2490112)
+                                .setTimestamp(new Date())
+                                .attachFile(file)
+                                .setImage('attachment://shipp.png')
+                                .addField('Casal escolhido foi', `${mentions[0]} ${mentions[1]}`, true)
+                                .addField('Chances atuais do relacionamento', `${falas[Math.round(Math.random() * falas.length - 1)]}`, true);
+                            
+                        message.channel.send({embed});
+                    });
+                });
+            });
+        }
+        
--- index.js
+++ index.js
@@ -22,9 +22,9 @@ client.on("ready", () => {
                     "footer": {
                         "icon_url": "",
                         "text": ""
-          },
-          "thumbnail": {
-            "url": "https://cdn.discordapp.com/attachments/437667917228802048/438149080598970368/20180423_222806.jpg"
+                    },
+                    "thumbnail": {
+                        "url": ""
                     },
                     "image": {
                         "url": ""
@@ -37,6 +37,11 @@ client.on("ready", () => {
                     "fields": [{
                             "name": "Entrada",
                             "value": "| Bem-vindo ao discord do **Shield**",
+                            "inline": true
+                        },
+                        {
+                            "name": "Quantidade atual de users",
+                            "value": `| Discord do OlympusRP: ${client.users.size -1} |`,
                             "inline": true
                         }
                     ]
